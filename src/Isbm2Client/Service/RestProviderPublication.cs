663d779cb398ed67435affdf03c0fd34ddd4388f
Hamish Murphy <hamishmurphy@gmail.com>
2023-03-20 15:23:08 +1100

    RestConsumerPublication can be mocked

--- src/Isbm2Client/Service/RestProviderPublication.cs
+++ src/Isbm2Client/Service/RestProviderPublication.cs
@@ -9,17 +9,17 @@ namespace Isbm2Client.Service;
 
 public class RestProviderPublication : AbstractRestService, IProviderPublication
 {
-    private readonly RestApi.IProviderPublicationServiceApi _publicationApi;
+    private readonly RestApi.IProviderPublicationServiceApi _providerApi;
 
-    public RestProviderPublication(RestApi.IProviderPublicationServiceApi publicationApi)
+    public RestProviderPublication(RestApi.IProviderPublicationServiceApi providerApi)
     {
-        _publicationApi = publicationApi;
-        _publicationApi.ExceptionFactory = IsbmFaultRestExtensions.IsbmFaultFactory;
+        _providerApi = providerApi;
+        _providerApi.ExceptionFactory = IsbmFaultRestExtensions.IsbmFaultFactory;
     }
 
     public async Task<PublicationProviderSession> OpenSession(string channelUri)
     {
-        var session = await ProtectedApiCallAsync( async () =>  await _publicationApi.OpenPublicationSessionAsync( channelUri ) );
+        var session = await ProtectedApiCallAsync( async () =>  await _providerApi.OpenPublicationSessionAsync( channelUri ) );
 
         return new PublicationProviderSession( session.SessionId );
     }
@@ -42,18 +42,18 @@ public class RestProviderPublication : AbstractRestService, IProviderPublication
             expiry: expiry
         );
 
-        var message = await ProtectedApiCallAsync( async () => await _publicationApi.PostPublicationAsync( sessionId, restMessage ) );
+        var message = await ProtectedApiCallAsync( async () => await _providerApi.PostPublicationAsync( sessionId, restMessage ) );
 
         return new PublicationMessage( message.MessageId, messageContent, topics.ToArray(), expiry );
     }
 
     public async Task ExpirePublication(string sessionId, string messageId)
     {
-        await ProtectedApiCallAsync( async () => await _publicationApi.ExpirePublicationAsync( sessionId, messageId ) );
+        await ProtectedApiCallAsync( async () => await _providerApi.ExpirePublicationAsync( sessionId, messageId ) );
     }
 
     public async Task CloseSession(string sessionId)
     {
-        await ProtectedApiCallAsync( async () => await _publicationApi.CloseSessionAsync(sessionId) );
+        await ProtectedApiCallAsync( async () => await _providerApi.CloseSessionAsync(sessionId) );
     }
 }
